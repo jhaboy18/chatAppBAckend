2964c1d7851699699582724a55c4ac38e3fc5a43
Raunak Jha <122020700+jhaboy18@users.noreply.github.com>
2026-06-10 11:01:20 +0530

    Update shonaka1stRasta.routes.js

--- shonaKaRastaa/shonaka1stRasta.routes.js
+++ shonaKaRastaa/shonaka1stRasta.routes.js
@@ -4,19 +4,19 @@ import { shonaKoprotectkro } from '../shona_Authrization/shonaw.middleware.js';
 
 const router = express.Router();
 
-// 🔐 Signup
+//
 router.post('/signup', signup);
 
-// 🔐 Login
+// 
 router.post('/login', login);
 
-// 👤 Get current user
+// 
 router.get('/me', shonaKoprotectkro, getMe);
 
-// 🚪 Logout
+// 
 router.post('/logout', shonaKoprotectkro, logout);
 
-//shona ko update krwao
+//
 
 router.put('/update',shonaKoprotectkro,updateProfile)
 
